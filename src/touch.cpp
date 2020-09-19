3e2f0b301740586caf4da612d825396b7aa58a4d
wakwak_koba <5591750+wakwak-koba@users.noreply.github.com>
2020-09-19 16:51:31 +0900

    Enhancement to HotZone
    - Fixed to be able to define multiple HotZone
    -Add events to each HotZone

--- src/touch.cpp
+++ src/touch.cpp
@@ -1,8 +1,14 @@
 #include "touch.h"
 
+volatile unsigned long touch::state = 0;
+
 touch::touch(/* args */)
 {
     //Wire1.begin(21,22);  
+    addHotZone(&baseZone);
+    addHotZone(&BtnA);
+    addHotZone(&BtnB);
+    addHotZone(&BtnC);    
 }
 touch::~touch()
 {
@@ -13,6 +19,15 @@ void touch::begin()
 {
     Wire1.begin(21,22);  
     pinMode(39, INPUT);
+    attachInterrupt(39, [] {
+        unsigned long ticks = ((millis() >> 1) << 1) | (digitalRead(39) & 0x01);
+        touch::state = ticks;
+    }, CHANGE);
+
+    Wire1.beginTransmission(CST_DEVICE_ADDR);
+    Wire1.write(0xA4);
+    Wire1.write(0x00);        // Interrupt Polling mode
+    Wire1.endTransmission();
 }
 
 bool touch::ispressed()
@@ -69,4 +84,272 @@ int touch::readTouchtoBuff(uint16_t *posx, uint16_t *posy)
         return (buff[1] >> 6) & 0x03;
     }
     return 0;
+}
+
+HotZoneRect *touch::createHotZone(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h)
+{
+    return new HotZoneRect(x0, y0, x0 + w - 1, y0 + h - 1);
+}
+
+HotZoneCircle* touch::createHotZone(uint16_t x, uint16_t y, uint16_t r)
+{
+    return new HotZoneCircle(x, y, r);
+}
+
+HotZoneTriangle* touch::createHotZone(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
+{
+    return new HotZoneTriangle(x0, y0, x1, y1, x2, y2);
+}
+
+HotZoneRect *touch::addHotZone(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h)
+{
+    HotZoneRect* zone = new HotZoneRect(x0, y0, x0 + w - 1, y0 + h - 1);
+    if(addHotZone(zone))
+        return zone;
+    delete zone;
+    return nullptr;
+}
+
+HotZoneCircle* touch::addHotZone(uint16_t x, uint16_t y, uint16_t r)
+{
+    HotZoneCircle* zone = new HotZoneCircle(x, y, r);
+    if(addHotZone(zone))
+        return zone;
+    delete zone;
+    return nullptr;
+}
+
+HotZoneTriangle* touch::addHotZone(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
+{
+    HotZoneTriangle* zone = new HotZoneTriangle(x0, y0, x1, y1, x2, y2);
+    if(addHotZone(zone))
+        return zone;
+    delete zone;
+    return nullptr;
+}
+
+int touch::readTouchtoBuff(TouchPoint_t *point1, TouchPoint_t *point2)
+{
+    uint8_t buff[16];
+    Wire1.beginTransmission(CST_DEVICE_ADDR);
+    Wire1.write(0x00);
+    if (Wire1.endTransmission() != 0)
+        return -1;
+    Wire1.requestFrom(CST_DEVICE_ADDR, sizeof(buff));
+    for (int i = 0; i < sizeof(buff); i++)
+        buff[i] = Wire1.read();
+    
+    unsigned long ticks = millis();
+    if (point1)
+    {
+        point1->x = ((buff[3] << 8) | buff[4]) & 0x0fff;
+        point1->y = ((buff[5] << 8) | buff[6]) & 0x0fff;
+        point1->event = (buff[3] >> 6) & 0x03;
+        point1->ticks = ticks;
+    }
+    if (point2)
+    {
+        point2->x = ((buff[9] << 8) | buff[10]) & 0x0fff;
+        point2->y = ((buff[11] << 8) | buff[12]) & 0x0fff;
+        point2->event = (buff[9] >> 6) & 0x03;
+        point2->ticks = ticks;
+    }
+    return buff[2];
+}
+
+int touch::calcDistance(TouchPoint_t* pos1, TouchPoint_t* pos2)
+{
+    // root2 は取ってない
+    return (pos1->x - pos2->x) * (pos1->x - pos2->x) + (pos1->y - pos2->y) * (pos1->y - pos2->y);
+}
+
+bool touch::addHotZone(HotZone* zone)
+{
+    m_hotZones.push_front(zone);
+    return true;
+}
+
+bool touch::removeHotZone(HotZone* zone, bool del)
+{
+    for(auto it = m_hotZones.begin(); it != m_hotZones.end(); ++it) {
+        if(*it == zone) {
+            m_hotZones.erase(it);
+            if(del)
+                delete *it;
+            return true;
+        }
+    }
+    return false;
+}
+
+void touch::handle()
+{
+    static unsigned long last_state = 0;
+    unsigned long now_state = state;
+   
+    // 暫定
+    if((now_state & 0x01) == 0)
+        return;
+    
+    if(last_state != now_state)
+    {
+        static int last_count;
+        
+        struct touchPoint {
+            TouchPoint_t point;
+            std::deque<TouchPoint_t>* locus = nullptr;
+        };
+        touchPoint nowTouch[2];
+        bool clear[2] = {false, false};
+        int now_count = readTouchtoBuff(&nowTouch[0].point, &nowTouch[1].point);
+        
+        // 軌跡を格納する
+        if(now_count == 2)    {
+            if(m_locus[0].size() == 0 && m_locus[1].size() == 0)    {
+                // 0指→2指
+                nowTouch[0].locus = &m_locus[0];
+                nowTouch[1].locus = &m_locus[1];
+                
+            } else if(m_locus[0].size() > 0)    {
+                // 1指→2指
+                auto last_p1 = m_locus[0].end();
+                int d1 = calcDistance(&(*last_p1), &nowTouch[0].point);
+                int d2 = calcDistance(&(*last_p1), &nowTouch[1].point);
+                if(d1 <= d2)    {
+                    nowTouch[0].locus = &m_locus[0];
+                    nowTouch[1].locus = &m_locus[1];
+                } else {
+                    nowTouch[0].locus = &m_locus[1];
+                    nowTouch[1].locus = &m_locus[0];
+                }
+            } else if(m_locus[1].size() > 0)    {
+                // 1指→2指
+                auto last_p2 = m_locus[1].end();
+                int d1 = calcDistance(&(*last_p2), &nowTouch[0].point);
+                int d2 = calcDistance(&(*last_p2), &nowTouch[1].point);
+                if(d1 <= d2)    {
+                    nowTouch[0].locus = &m_locus[1];
+                    nowTouch[1].locus = &m_locus[0];
+                } else {
+                    nowTouch[0].locus = &m_locus[0];
+                    nowTouch[1].locus = &m_locus[1];
+                }
+            }
+        } else if(now_count == 1)    {
+            if(m_locus[0].size() == 0 && m_locus[1].size() == 0)    {
+                // 0指→1指
+                nowTouch[0].locus = &m_locus[0];
+            } else if (m_locus[0].size() > 0 && m_locus[1].size() > 0)    {
+                // 2指→1指(近い方を探す)
+                auto last_p1 = m_locus[0].end();
+                auto last_p2 = m_locus[1].end();
+                auto d1 = calcDistance(&(*last_p1), &nowTouch[0].point);
+                auto d2 = calcDistance(&(*last_p2), &nowTouch[0].point);
+                if(d1 <= d2)    {
+                    nowTouch[0].locus = &m_locus[0];
+                    nowTouch[1].locus = &m_locus[1];
+                    clear[1] = true;
+                } else {
+                    nowTouch[0].locus = &m_locus[1];
+                    nowTouch[1].locus = &m_locus[0];
+                    clear[0] = true;
+                }
+            } else if(m_locus[0].size() > 0)
+                // 1指→1指
+                nowTouch[0].locus = &m_locus[0];
+            else if(m_locus[1].size() > 0)
+                nowTouch[0].locus = &m_locus[1];
+        } else if(now_count == 0)    {
+            if(m_locus[0].size() > 0 && m_locus[1].size() > 0)    {
+                // 2指→0指
+                nowTouch[0].locus = &m_locus[0];
+                nowTouch[1].locus = &m_locus[1];
+            } else if(m_locus[0].size() > 0)    {
+                // 1指→0指
+                nowTouch[0].locus = &m_locus[0];
+            }
+            
+            // N指→0指
+            clear[0] = true;
+            clear[1] = true;
+        }
+        
+        for(int i = 0; i < 2; i++)    {
+            if(nowTouch[i].locus != nullptr)    {
+                if(nowTouch[i].locus->size() > 0)    {
+                    auto last_p = nowTouch[i].locus->end();
+                    if((*last_p).x != nowTouch[i].point.x || (*last_p).y != nowTouch[i].point.y)
+                        nowTouch[i].locus->push_back(nowTouch[i].point);
+                } else
+                    nowTouch[i].locus->push_back(nowTouch[i].point);
+            }
+        }
+        
+        for(int i = 0; i < (last_count > now_count ? last_count : now_count); i++)    {
+            for (auto it = m_hotZones.begin(); it != m_hotZones.end(); ++it)    {
+                HotZone::eventArgs_T e;
+                e.point = nowTouch[i].point;
+                
+                if(nowTouch[i].locus != nullptr)    {
+                    for (auto itp = nowTouch[i].locus->begin(); itp != nowTouch[i].locus->end(); ++itp)    {
+                        if((*it)->inHotZone((*itp)))
+                            e.locusPoints.push_back(&(*itp));
+                    }
+                }
+                
+                // onMove
+                    if((*it) != nullptr && (*it)->enabled && (*it)->inHotZone(e.point) && (*it)->onMove)
+                    {
+                        (*it)->onMove((*it), &e);
+                        if(e.suppress)
+                            break;
+                    }
+                    if((*it) != nullptr && (*it)->inHotZone(e.point) && this->onMove)
+                    {
+                        this->onMove((*it), &e);
+                        if(e.suppress)
+                            break;
+                    }
+                    if((*it) != nullptr && (*it)->enabled)
+                        (*it)->inHotZoneDoFun(e.point);
+
+                if(now_count != last_count)    {
+                    // onClick
+                    if((*it) != nullptr && (*it)->enabled && (*it)->inHotZone(e.point) && (*it)->onClick)
+                    {
+                        if(e.point.event == 1) {
+                            auto f = e.locusPoints.front();
+                            if(f->event == 0) {
+                                (*it)->onClick((*it), &e);
+                                if(e.suppress)
+                                    break;
+                            }
+                        }
+                    }
+                    
+                    // onPressChanged
+                    if((*it) != nullptr && (*it)->enabled && (*it)->inHotZone(e.point) && (*it)->onPressChanged)
+                    {
+                        (*it)->onPressChanged((*it), &e);
+                        if(e.suppress)
+                            break;
+                    }
+                    if((*it) != nullptr && (*it)->inHotZone(e.point) && this->onPressChanged)
+                    {
+                        this->onPressChanged((*it), &e);
+                        if(e.suppress)
+                            break;
+                    }
+                }
+            }
+        }
+        last_count = now_count;
+        last_state = now_state;
+        
+        // clearLocus
+        for(int i = 0; i < 2; i++)
+            if(clear[i])
+                m_locus[i].clear();
+        
+    }
 }
--- src/touch.h
+++ src/touch.h
@@ -3,6 +3,9 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <functional>
+#undef min
+#include <deque>
 
 #define CST_DEVICE_ADDR 0x38
 
@@ -10,17 +13,67 @@ typedef struct point
 {
     int x;
     int y;
+    int event = -1;        // 0:PressDown 1:LiftUp 2:coodinate
+    unsigned long ticks;
+    
+    inline std::string toString() {
+        std::string res = "position:(";
+        char val[10];
+        snprintf(val, sizeof(val), "%03d,%03d", x, y);
+        res += val;
+        res += ")";
+        res += ",event:";
+        snprintf(val, sizeof(val), "%d", event);
+        res += val;
+        res += ",ticks:";
+        snprintf(val, sizeof(val), "%lu", ticks);
+        res += val;
+        return res;
+    }
 }TouchPoint_t;
 
 typedef struct HotZone
 {
-    HotZone(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, void (*fun)() = nullptr){
+    virtual inline bool inHotZone(TouchPoint_t point) = 0;
+    virtual inline bool inHotZoneDoFun(TouchPoint_t point)
+    {
+        if(_fun != nullptr && inHotZone(point)) {
+            _fun();
+            return true;
+        }
+        return false;
+    }
+
+    bool               enabled = true;
+    std::string        name;
+    void (*_fun)();
+
+    typedef struct {
+        TouchPoint_t point;
+        std::deque<TouchPoint_t*> locusPoints;
+        bool    suppress = true;        
+    }eventArgs_T;
+    
+    std::function<void(HotZone*, eventArgs_T*)> onClick;
+    std::function<void(HotZone*, eventArgs_T*)> onPressChanged;
+    std::function<void(HotZone*, eventArgs_T*)> onMove;
+    
+    virtual inline std::string toString() = 0;
+
+}HotZone_t;
+
+typedef struct HotZoneRect : public HotZone
+{
+    HotZoneRect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, void (*fun)() = nullptr){
         _x0 = x0; 
         _y0 = y0;
         _x1 = x1;
         _y1 = y1;
         _fun = fun;
 
+    }
+    HotZoneRect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, std::string name) : HotZoneRect(x0, y0, x1, y1) {
+        this->name = name;
     }
     inline void setZone(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, void (*fun)() = nullptr )
     {
@@ -30,24 +83,11 @@ typedef struct HotZone
         _y1 = y1;
         _fun = fun;
     }
-    inline bool inHotZone(TouchPoint_t point)
+    virtual inline bool inHotZone(TouchPoint_t point)
     {
         if(( point.x >= _x0 )&&( point.x <=  _x1 )&&
            ( point.y >= _y0 )&&( point.y <=  _y1 ))
         {
-            return true;
-        }
-        return false;
-    }
-    inline bool inHotZoneDoFun(TouchPoint_t point)
-    {
-        if(( point.x >= _x0 )&&( point.x <=  _x1 )&&
-           ( point.y >= _y0 )&&( point.y <=  _y1 ))
-        {
-            if( _fun != nullptr )
-            {
-                _fun();
-            }
             return true;
         }
         return false;
@@ -57,14 +97,111 @@ typedef struct HotZone
     uint16_t _x1;
     uint16_t _y1;
 
-    void (*_fun)();
+    virtual inline std::string toString() {
+        std::string res = "rect:(";
+        char val[8];
+        snprintf(val, sizeof(val), "%03d,%03d", _x0, _y0);
+        res += val;
+        res += ")-(";
+        snprintf(val, sizeof(val), "%03d,%03d", _x1, _y1);
+        res += val;
+        res += ")";
+        return res;
+    }
+};
 
-}HotZone_t;
+typedef struct HotZoneCircle : public HotZone
+{
+    HotZoneCircle(uint16_t x, uint16_t y, uint16_t r, void (*fun)() = nullptr){
+        this->x = x;
+        this->y = y;
+        this->r = r;
+        _fun = fun;
+
+    }
+    HotZoneCircle(uint16_t x, uint16_t y, uint16_t r, std::string name) : HotZoneCircle(x, y, r) {
+        this->name = name;
+    }
+
+    virtual inline bool inHotZone(TouchPoint_t point)
+    {
+        if((point.x - x) * (point.x - x) + (point.y - y) * (point.y - y) <= r * r)
+            return true;
+
+        return false;
+    }
+
+    uint16_t x;
+    uint16_t y;
+    uint16_t r;
+
+    virtual inline std::string toString() {
+        std::string res = "circle:(";
+        char val[12];
+        snprintf(val, sizeof(val), "%03d,%03d,%03d", x, y, r);
+        res += val;
+        res += ")";
+        return res;
+    }
+};
+
+typedef struct HotZoneTriangle : public HotZone
+{
+    HotZoneTriangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, void (*fun)() = nullptr){
+        this->x0 = x0;
+        this->y0 = y0;
+        this->x1 = x1;
+        this->y1 = y1;
+        this->x2 = x2;
+        this->y2 = y2;
+        _fun = fun;
+
+    }
+    HotZoneTriangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, std::string name) : HotZoneTriangle(x0, y0, x1, y1, x2, y2) {
+       this->name = name;
+    }
+
+    virtual inline bool inHotZone(TouchPoint_t point)
+    {
+        double a = 0.5 * (-y1 * x2 + y0 * (-x1 + x2) + x0 * (y1 - y2) + x1 * y2);
+        double s = 1 / (2 * a) * (y0 * x2 - x0 * y2 + (y2 - y0) * point.x + (x0 - x2) * point.y);
+        double t = 1 / (2 * a) * (x0 * y1 - y0 * x1 + (y0 - y1) * point.x + (x1 - x0) * point.y);
+        
+        if((0 < s < 1) && (0 < t < 1) && (0 < 1 - s - t < 1))
+            return true;
+        
+        return false;
+    }
+
+    uint16_t x0;
+    uint16_t y0;
+    uint16_t x1;
+    uint16_t y1;
+    uint16_t x2;
+    uint16_t y2;
+
+    virtual inline std::string toString() {
+        std::string res = "triangle:(";
+        char val[8];
+        snprintf(val, sizeof(val), "%03d,%03d", x0, y0);
+        res += val;
+        res += ")-(";
+        snprintf(val, sizeof(val), "%03d,%03d", x1, y1);
+        res += val;
+        res += ")-(";
+        snprintf(val, sizeof(val), "%03d,%03d", x2, y2);
+        res += val;
+        res += ")";
+        return res;
+    }
+};
 
 class touch
 {
 private:
     /* data */
+
+    volatile static unsigned long state;
 public:
     
     TouchPoint_t _TouchPoint;
@@ -75,9 +212,36 @@ public:
     void begin();
     bool ispressed();
     TouchPoint_t getPressPoint();
-    HotZone_t* creatHotZone(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h);
+    HotZoneRect* createHotZone(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h);
 private:
     int readTouchtoBuff(uint16_t *posx, uint16_t *posy);
+
+public:
+    std::function<void(HotZone*, HotZone::eventArgs_T*)> onPressChanged;
+    std::function<void(HotZone*, HotZone::eventArgs_T*)> onMove;
+    
+    HotZoneRect BtnA    {  0, 240,        119, UINT16_MAX, "buttonA"};
+    HotZoneRect BtnB    {180, 240,        209, UINT16_MAX, "buttonB"};
+    HotZoneRect BtnC    {240, 240, UINT16_MAX, UINT16_MAX, "buttonC"};
+
+    HotZoneCircle* createHotZone(uint16_t x, uint16_t y, uint16_t r);
+    HotZoneTriangle* createHotZone(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+
+    HotZoneRect* addHotZone(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h);
+    HotZoneCircle* addHotZone(uint16_t x, uint16_t y, uint16_t r);
+    HotZoneTriangle* addHotZone(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+
+    bool addHotZone(HotZone* zone);
+    bool removeHotZone(HotZone* zone, bool del = true);
+    void handle();
+
+private:
+    HotZoneRect baseZone {0, 0, UINT16_MAX, 239, "somewhere"};
+    std::deque<HotZone*> m_hotZones;
+    std::deque<TouchPoint_t> m_locus[2];
+
+    int readTouchtoBuff(TouchPoint_t *point1, TouchPoint_t *point2);
+    static int calcDistance(TouchPoint_t* pos1, TouchPoint_t* pos2);
 };
 
 
